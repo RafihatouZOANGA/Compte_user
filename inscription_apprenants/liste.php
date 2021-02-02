efe9d4919e89c646fcd68fbe7b8041240fceb923
Rafihatou <rafihazoanga7@gmail.com>
2021-02-02 08:27:24 +0000

    mon deuxieme commit

---
+++ inscription_apprenants/liste.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Création de compte User</title>
+    <link rel="stylesheet" href="style/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <!-- début entete-->
+    <header> 
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <h4 class="MyApp" >MyApp</h4>
+                <div class="div_lien_liste a_liste">Projet Apprenants Simplon</div>
+                <img  class="img-fluid" height="40px" width="200px" src="style/Image/logo br.png" alt=""> 
+            </div>
+          </nav>
+    <!-- fin entete-->
+    </header> 
+        <!-- début corps-->
+        <button class="sign_in"> <a href="formulaire.html">Retour</a> </button>
+        <!-- fin corps-->
+</body>
+
+<footer>
+    <div class="container-fluid bg-dark">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4">
+               <img src="style/Images/co.png" width="55" height="55" alt=""/>
+               <span style="color: white">SIMPLON 2021</span>
+            </div>
+            <div class="col-4"></div> 
+        </div>
+    </div>
+</footer>
+</html>
--- validation_creation.php
+++ validation_creation.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Création de compte User</title>
     <link rel="stylesheet" href="style/bootstrap-4.5.3-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/style2.css">
+    <link rel="stylesheet" href="style/styleUser.css">
 </head>
 <body>
     <!-- début entete-->
@@ -15,7 +15,7 @@
             <div class="container">
                 <h4 class="MyApp" >MyApp</h4>
                 <div class="div_lien_liste a_liste">Projet Apprenants Simplon</div>
-                <img  class="img-fluid" height="40px" width="200px" src="style/images/logo br.png" alt=""> 
+                <img  class="img-fluid" height="40px" width="200px" src="../images/index.jpg"  alt="emoticon"> 
             </div>
           </nav>
     <!-- fin entete-->
@@ -24,31 +24,25 @@
     <div class="container">
     <div class="row form_compte">
             <div class="col-4"></div>
-            <div class="col-4-fluid">
+            <div class="col-4-fluid cadre">
+                <h3 class="inscription" ><strong>Félicitation!</strong></h3>
+                <p>Vous venez de créer un compte</p>
+                <p class="avant_sign"><a href="identification.html">Veuillez vous connecter svp.</a></p>
                 <h3 class="inscription" ><strong>Créer un compte</strong></h3>
-                <form action="" method="post">
+                <form action="traitementUser.php" method="post">
+                    <input class="input_compte" type="text" name="pseudo" placeholder="Pseudo"> <br>
                     <input class="input_compte" type="email" name="mail" placeholder="E-mail"> <br>
                     <input class="input_compte" type="password" name="password" placeholder="Mot de passe"> <br>
-                    <input class="submit" type="submit" value="Envoyer">
+                    <input class="submit" type="submit" value="Valider">
                 </form>
-                <p>Avez-vous déjà un compte?</p>
                 <div class="sign">
-                    <button class="sign_in"> <a href="identification.html">Se connecter</a> </button>
+                    <button class="sign_in">Se connecter</button>
                 </div>
             </div>
             <div class="col-4"></div>
         </div>
     </div>
         <!-- fin entete-->
-
-
-    
-<!--  la div recherche.    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>-->
 
 </body>
 
